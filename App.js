49e9389f0592eba238842b60aee6cf49dcd0b2a1
VuLanTuong <lantuonglearning@gmail.com>
2024-01-04 22:51:20 +0700

    set up bottom tab

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+import FriendsScreen from './screens/friends-screen';
+import MessageScreen from './screens/message-screen';
+import ProfileScreen from './screens/profile-screen';
+import { NavigationContainer } from '@react-navigation/native';
+
+function MyTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Message" component={FriendsScreen} />
+      <Tab.Screen name="Contact" component={MessageScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTab />
+    </NavigationContainer>
   );
 }
 
---
+++ screens/friends-screen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MessageScreen() {
+    return (
+        <View>
+            <Text>Friend Screen</Text>
+        </View>
+    )
+}
