522c9ebf79ad357b36436e40380745d6cea427d1
VuLanTuong <lantuonglearning@gmail.com>
2024-01-05 11:46:35 +0700

    completed header and bottom tab

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab = createBottomTabNavigator();
 import FriendsScreen from './screens/friends-screen';
 import MessageScreen from './screens/message-screen';
 import ProfileScreen from './screens/profile-screen';
 import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function MyTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Message" component={FriendsScreen} />
-      <Tab.Screen name="Contact" component={MessageScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Messgae"
+      screenOptions={{
+        headerStyle: {
+          flexDirection: 'row',
+          justifyContent: 'center',
+          backgroundColor: '#f558a4',
+        },
+        headerRight: () => (
+          <View style={{ flexDirection: 'row', width: '100%' }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', gap: 15 }}>
+              <MaterialCommunityIcons name="magnify" color="white" size={30} />
+              <TextInput
+                placeholder="Search"
+                placeholderTextColor="white"
+                // value={searchKeyword}
+                // onSubmitEditing={() => navigation.navigate('SearchEngine', { keyword: searchKeyword })}
+                // onChangeText={text => setSearchKeyword(text)}
+                style={{ height: 25, fontSize: 20, color: 'white' }}
+              />
+              <MaterialCommunityIcons name="plus" color="white" size={30} style={{
+                marginLeft: 50
+              }} />
+            </View>
+          </View>
+        ),
+      }}>
+      <Tab.Screen options={{
+        headerTitle: '',
+        tabBarLabel: 'Message',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name='message-text' color={'#f558a4'} size={size} />
+        ),
+      }} name="Message" component={FriendsScreen} />
+      <Tab.Screen options={{
+        headerTitle: '',
+        tabBarLabel: 'Contact',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name='notebook' color={'#f558a4'} size={size} />
+        ),
+      }} name="Contact" component={MessageScreen} />
+      <Tab.Screen options={{
+        headerTitle: '',
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name='account' color={'#f558a4'} size={size} />
+        ),
+      }} name="Profile" component={ProfileScreen} />
+    </Tab.Navigator >
   )
 }
 
